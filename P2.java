8f9f6f5575378d4477ef32b9a9af4ba36d6cee82
Bryan Reilly <94bryanr@gmail.com>
2014-09-26 12:11:06 -0600

    Updated P2 Main method, not yet tested. Should work as expected

--- P2.java
+++ P2.java
@@ -1,33 +1,45 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class P2 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         //Object to hold word information for given web pages.
         WebPages webPage = new WebPages();
-        webPage.addPage("test.txt");
+        //Commands from input file
+        ArrayList<String> commands = parseInputFile(args[0]);
+        //Scan in files until *EOF* command
+        boolean reachedEOF = false;
+        for (String command : commands) {
+            if (command.equals("*EOFs*"))
+                reachedEOF = true;
+            if (!reachedEOF) {
+                webPage.addPage(command);
+                webPage.printTerms();
+            }
+            if (reachedEOF) {
+                for (String appearence : webPage.whichPages(command)) {
+                    System.out.println(appearence);
+                }
+            }
 
-        //Prints each word
-        webPage.printTerms();
-
-        //Removes *n* most common words
-        //webPage.pruneStopWords(2);
-
-        //Prints each word
-        //webPage.printTerms();
-
-        for(String appearance: webPage.whichPages("tiger")){
-            System.out.println(appearance);
+            webPage.pruneStopWords(2);
         }
-
-
-        // --Pseudocode to implement--
-        //for each word in input file,
-        //  run whichPages method on webpages object
-        //      if(found word)
-        //          print “<word> in pages: <page1>, <page2>…”
-        //      else
-        //          print “<word> not found”
     }
 
-    public void parseInputFile(){
+    public static ArrayList<String> parseInputFile(String filename) {
+        ArrayList<String> commands = new ArrayList<String>();
 
+        File input = new File(filename);
+        try {
+            Scanner scanner = new Scanner(input);
+            while (scanner.hasNextLine()) {
+                commands.add(scanner.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return commands;
     }
 }
