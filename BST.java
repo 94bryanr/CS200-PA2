3945a2836881a2bd4bc7e922fca036609740b122
Garrett Phoonswadi <garrett.phoonswadi@gmail.com>
2014-10-17 09:29:51 -0600

    added BST class

---
+++ BST.java
@@ -0,0 +1,29 @@
+
+public class BST {
+    // intialize private count and root variables
+    private int count;
+    private BST root; // middle branch node
+    private BST leftchild; // left and right branches
+    private BST rightchild;
+
+    public BST(){
+        this.count =0;
+        root = null;
+    }
+    public int size(){
+        return count;
+    }
+
+    public void add(String documentName, String word){
+        //adds a new term or inc frequency if it already exists in BST
+
+    }
+    public Term get(String word, Boolean printDepth){
+        // returns the name of the term
+        // checks to see if printDepth is true and if so then checks to see how deep it exists in the tree 
+    }
+    public void delete(String word){
+        //takes terms out of the BST
+
+    }
+}
